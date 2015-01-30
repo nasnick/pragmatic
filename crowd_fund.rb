a8399a6544d99fd4f650faa408e8142eb51badf9
nasnick <nas.nick@gmail.com>
2015-01-31 11:45:00 +1300

    Jan 31st

---
+++ crowd_fund.rb
@@ -0,0 +1,9 @@
+project1="ABC"
+project2="LMN"
+project3="XYZ"
+
+project1_funding = 1000
+
+puts "Project #{project1} has $#{project1_funding} in funding.\n\n"
+
+puts "Projects:\n\tProject #{project1}\n\tProject #{project2}\n\tProject #{project3}"
--- studio_game.rb
+++ studio_game.rb
@@ -1,9 +1,28 @@
 name1 = "larry"
 name2 = "curly"
 name3 = "moe"
+name4 = "shemp"
 
-name1_health = 60
+health1 = 60
+health2 = 125
+health3 = 100
+health4 = 90
 
-puts "#{name1}'s health is #{name1_health / 9.0}"
+puts "#{name1.capitalize} has a health of #{health1}"
+puts "#{name2.upcase} has a health of #{health2}"
+name3_sentence = "#{name3.upcase} has a health of #{health3}"
+puts name3_sentence.center(48, '*')
+puts "#{name4.capitalize.ljust(20, '.')} #{health4} health"
+puts "\n"
 
 puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+puts "\n"
+
+health2 = health1
+
+# current_time = Time.new
+# formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+# puts "The game started on #{formatted_time}"
+
+day_date_time = Time.new.strftime("The game started on %A %D at %I:%M%p")
+puts "#{day_date_time}"
