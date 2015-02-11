bdd7423d0f523e4fe206a0d986e23f9c079b8eb7
nasnick <nas.nick@gmail.com>
2015-02-12 12:07:27 +1300

    Update game_spec.rb

--- game/game_spec.rb
+++ game/game_spec.rb
@@ -1,28 +1,31 @@
-require_relative 'game'
-require_relative 'die'
+require_relative 'game' 	
+
 describe Game do
   before do
+    $stdout = StringIO.new
     @game = Game.new("knuckleheads")
     
     @initial_health = 100
-    @player = Player.new("moe", @initial_health)
+    @player = Player.new("joe", @initial_health)
     
     @game.add_player(@player)
-    $stdout = StringIO.new  
-  end
+
+  end 
   it "should w00t a player if a high number is rolled" do
     Die.any_instance.stub(:roll).and_return(5)
     @game.play
-    @player.health.should == @initial_health + 15
+    @player.health.should == @initial_health  + 15
   end
-  it "health should stay the same if mid number rolled on die" do
+
+  it "should blam a player if a low number is rolled" do
+    Die.any_instance.stub(:roll).and_return(2)
+    @game.play
+    @player.health.should == @initial_health - 10
+  end
+
+  it "should do nothing if a medium number is rolled" do
     Die.any_instance.stub(:roll).and_return(3)
     @game.play
     @player.health.should == @initial_health
-  end
-  it "health should decrease by 10 if 1..2 rolled" do
-    Die.any_instance.stub(:roll).and_return(1)
-    @game.play
-    @player.health.should == @initial_health - 10
   end
 end
